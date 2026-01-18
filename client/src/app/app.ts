c34ca6ed06280bc19210953a8b46f98c98affb30
ikrishnendu <krishnendusahoo.edu@gmail.com>
2026-01-18 20:55:35 +0530

    home page completed

--- client/src/app/app.ts
+++ client/src/app/app.ts
@@ -4,9 +4,7 @@ import { RouterOutlet } from '@angular/router';
 @Component({
   selector: 'app-root',
   imports: [RouterOutlet],
-  templateUrl: './app.html',
-  styleUrl: './app.css'
+  template: ` <router-outlet></router-outlet> `,
+  styleUrl: './app.css',
 })
-export class App {
-  protected readonly title = signal('client');
-}
+export class App {}
